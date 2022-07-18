33a7697e413c2f6d130708b18c7e17e8e655f5a0
Neely Arline <neelyarl@su-d5.msk.21-school.ru>
2022-07-18 20:22:36 +0300

    dasha ne dala dodelat'

--- src/s21_decimal_converters.c
+++ src/s21_decimal_converters.c
@@ -13,7 +13,13 @@ int s21_from_int_to_decimal(int src, s21_decimal *dst) {
 }
 
 int s21_from_float_to_decimal(float src, s21_decimal *dst) {
-
+    int convertError = 0;
+    if (dst) {
+        
+    } else {
+        convertError = 1;
+    }
+    return convertError;
 }
 
 int s21_from_decimal_to_int(s21_decimal src, int *dst) {
